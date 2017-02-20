d8affcf1be54cf6fc1405d288ad9a3637d35d109
Sahand Kashani-Akhavan <sahand.kashani@gmail.com>
2017-02-20 07:38:43 +0100

    Reuse code of stop.sh in start.sh. Add Progress report and error handling if no arguments provided to start.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-killall browser-wsconfig
-killall browser-wsaudio
-killall browser-main-daemon
+killall browser-main-daemon > /dev/null 2>&1
+killall browser-wsaudio     > /dev/null 2>&1
+killall browser-wsconfig    > /dev/null 2>&1
 
 echo "Daemons stopped"
